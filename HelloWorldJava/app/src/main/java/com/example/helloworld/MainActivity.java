04b7a6b23f15d724b4fa4e25305b9d6169901a5c
James Goodrich <jdgoodrichjr@gmail.com>
2019-03-04 17:02:39 -0600

    Finished the "Build Your First App" tutorial.
    Had a few ideas for a project I wanted to undertake this year, and this helped me draw some lines
    to getting some more work done on that front, plus a few sketches with a pen and paper helps too.
    
    "Look Ma, I'm an Android developer!" hahaha, baby steps, young padawan, baby steps...
    
    Ran into an issue, as I wasn't intially planning on putting this up on GH, but had to invalidate
    AS's cache, clean the project, and then rebuild the APK(s), to get it running on a donated Moto X
    (2013) again. SO (https://stackoverflow.com/questions/42219784/installation-failed-with-message-invalid-file)
    to the resucue!

---
+++ HelloWorldJava/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.helloworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.helloworld.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(Resources.mainActivityLayoutId());
+    }
+
+    /**
+     * Sends a message.
+     *
+     * @param view The view.
+     */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(Resources.editTextViewId());
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+}
